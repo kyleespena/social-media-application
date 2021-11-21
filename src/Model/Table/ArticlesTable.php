029af4722eae77a1dd787c82dd790e95555b0a76
kyleespena <jasyle92@gmail.com>
2021-11-21 21:02:43 +0800

    first commit

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model\Table;
+use Cake\Validation\Validator;
+use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->addBehavior('Timestamp');
+    }
+    
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
+    }
+}
